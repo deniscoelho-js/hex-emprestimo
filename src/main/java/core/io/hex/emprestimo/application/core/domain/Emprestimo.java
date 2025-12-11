e5728fbc4f71b04e64960eede00b72a50568f591
Denis Coelho <deniscoelho.dev@gmail.com>
2025-12-11 18:00:40 -0300

    atualizacao

--- src/main/java/core/io/hex/emprestimo/application/core/domain/Emprestimo.java
+++ src/main/java/core/io/hex/emprestimo/application/core/domain/Emprestimo.java
@@ -1,10 +1,18 @@
 package core.io.hex.emprestimo.application.core.domain;
 
 import core.io.hex.emprestimo.application.core.domain.enums.StatusContrato;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Emprestimo {
     private Long idParcela;
     private Long idPessoaJuridica;
--- src/main/java/core/io/hex/emprestimo/application/core/domain/Parcela.java
+++ src/main/java/core/io/hex/emprestimo/application/core/domain/Parcela.java
@@ -1,9 +1,17 @@
 package core.io.hex.emprestimo.application.core.domain;
 
 import core.io.hex.emprestimo.application.core.domain.enums.StatusParcela;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Parcela {
     private Long id;
     private Integer numeroParcela;
